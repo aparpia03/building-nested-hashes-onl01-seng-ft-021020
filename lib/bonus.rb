21d3985bb04f484e0acaefeb6c2a6bb71ddf5191
Abbas Parpia <aparpia03@gmail.com>
2020-02-17 16:38:13 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,4 +27,5 @@ return epic_tragedy[:capulet][:heroine][:status] => "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
+  end
 end
